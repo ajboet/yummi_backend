77e64f3ef0d649190af946c04b61b8799561b21d
ajboet <ajboet@hotmail.com>
2020-05-16 22:34:29 -0400

    Improving cookie usage, so it could be handled properly by frontend

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,9 +20,20 @@ class ProductController extends Controller
         if ($products->count() > 0) {// When Products present
             // Check if the cart cookie hasn't been set in client's browser
             if (! request()->cookie('yummi_cart')) {
+                $cookie_name = config('cart_manager.cookie_name');
+                $cookie_value = Str::random(20);
+                $cookie_expires = config('cart_manager.cookie_lifetime');
                 // Then send the cookie along with the Products
-                return response()->json($products, 200)
-                                 ->cookie('yummi_cart', Str::random(20));
+                return response()->json([
+                    'products' => $products,
+                    'cookie' => [
+                        'name' => $cookie_name,
+                        'value' => $cookie_value,
+                        'expires' => $cookie_expires
+                    ]
+                ], 200)
+                // Not an API working approach
+                ->cookie($cookie_name, $cookie_value, $cookie_expires, '', '', False, False);
             } else {
                 // Send the Products
                 return response()->json($products, 200);
